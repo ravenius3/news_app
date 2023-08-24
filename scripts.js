2e967f30eeea26dc6d5d2a62ef63c0f1c5cf6dfa
Rick Avenius <ravenius3@gmail.com>
2023-08-24 16:45:53 -0400

    fourth commit

--- scripts.js
+++ scripts.js
@@ -16,29 +16,52 @@ async function fetchNews() {
 }
 
 function displayNews(articles) {
-    const newsDiv = document.querySelector('#news')
+    const cardDeck = document.querySelector('#deck')
+    counter = 0;
 
     for (const article of articles) {
-        let title = document.createElement('h4');
-        title.textContent = article.title;
-        newsDiv.appendChild(title);
+        counter +=1;
+
+        let cardDiv = document.createElement('div')
+        cardDiv.className = 'card d-inline-block mb-2 me-2';
+        cardDiv.style = 'width: 18rem;'
+        cardDiv.id = `card${counter}`
+        cardDeck.appendChild(cardDiv);
+
+        let cardTarget = document.querySelector(`#card${counter}`);
 
         let img = document.createElement('img');
         img.src = article.urlToImage;
-        newsDiv.appendChild(img)
+        img.className = 'card-img-top';
+        img.alt = 'news article image'
+        cardTarget.appendChild(img)
 
-        let author = document.createElement('h6');
-        author.textContent = article.author;
-        newsDiv.appendChild(author);
+        let cBody = document.createElement('div')
+        cBody.className = 'card-body';
+        cBody.id = `cBody${counter}`;
+        cardTarget.appendChild(cBody);
 
-        let newsArticle = document.createElement('a')
-        newsArticle.textContent = 'Click here to view this article.'
-        newsArticle.href = article.url;
-        newsDiv.appendChild(newsArticle);
+        let cardBodyTarget = document.querySelector(`#cBody${counter}`);
+
+        let title = document.createElement('h5');
+        title.textContent = article.title;
+        title.className = 'card-title';
+        cardBodyTarget.appendChild(title);
+
+        let author = document.createElement('p');
+        author.textContent = `Written by: ${article.author}`;
+        author.className = 'card-text text-end'
+        cardBodyTarget.appendChild(author);
 
         let articleSource = document.createElement('p')
-        articleSource.id = 'source'
+        articleSource.className = 'card-text'
         articleSource.textContent = `Originally published by ${article.source.name}.`;
-        newsDiv.appendChild(articleSource);
+        cardBodyTarget.appendChild(articleSource);
+
+        let newsArticle = document.createElement('a')
+        newsArticle.textContent = 'Click to View';
+        newsArticle.href = article.url;
+        newsArticle.className = 'btn btn-primary';
+        cardBodyTarget.appendChild(newsArticle);
     }
 }
